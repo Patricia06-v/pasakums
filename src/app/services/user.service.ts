8433c8b688db9bb89f1cbbacc8dfc5c67b9b90e9
Patricia06-v <pbelasika@gmail.com>
2025-12-09 21:19:51 +0200

    e

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -12,31 +12,26 @@ export class UserService {
   private readonly URL = environment.apiUrl + '/users';
   private http = inject(HttpClient);
 
-  // POST: Lietotāja reģistrācija
   register(user: UserModel): Observable<any> {
     return this.http.post(`${this.URL}/register`, user, {
       observe: 'response'
     });
   }
 
-  // POST: Pieteikšanās
   login(user: UserModel): Observable<any> {
     return this.http.post(`${this.URL}/login`, user, {
       observe: 'response'
     });
   }
 
-  // GET: Lietotāja dati
   getUser(id: number): Observable<UserModel> {
     return this.http.get<UserModel>(`${this.URL}/${id}`);
   }
 
-  // GET: Lietotāja pasākumi
   getUserEvents(id: number): Observable<any> {
     return this.http.get(`${this.URL}/${id}/events`);
   }
 
-  // DELETE: Dzēst lietotāju
   deleteAccount(id: number): Observable<any> {
     return this.http.delete(`${this.URL}/${id}`, {
       observe: 'response'
